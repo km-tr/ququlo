5a5e632f66024e0b91fc26dc9d14323cd38fa074
Yuta Kumagai <y.kumagai@menew.jp>
2020-08-18 21:55:02 +0900

    change viewport

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -11,7 +11,7 @@ const Layout = ({ children, title = 'QUQULO' }: Props) => (
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta name="viewport" content="initial-scale=1.0, width=768" />
     </Head>
     {children}
   </>
