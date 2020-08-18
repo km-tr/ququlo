492df9bb9c4e1e7bd1d458af5bb52c40584652ac
Yuta Kumagai <y.kumagai@menew.jp>
2020-08-18 21:43:09 +0900

    adjust style

---
+++ src/pages/_app.tsx
@@ -0,0 +1,8 @@
+import {NextPage} from "next";
+import {AppProps} from "next/app";
+
+import "../styles.css"
+
+const App: NextPage<AppProps> = ({Component, pageProps}) => <Component {...pageProps} />;
+
+export default App
